c3c6ab85ddd45d88db0ce136ab2f9603e03afaa7
kkelar241p-max <student@student.machine>
2025-11-12 12:10:49 +0000

    studentmachine send

--- cpp/szablon/main.cpp
+++ cpp/szablon/main.cpp
@@ -3,9 +3,18 @@
 using namespace std;
 
 int main(){
-	for(int i=1; i<4; i++){
-		cout<<"*";
+	int x;
+	cout<<"Podaj x: ";
+	cin>>x;
+	if(x%21==0){
+		cout<<"Liczba jest podzielna przez 21"<<endl;
+	}else{
+		cout<<"Nie jest podzielna przez 21"<<endl;
 	}
-	cout<<endl;
+	if(x%10==0 || x%30==0 || x%50==0 || x%70==0 || x%90==0){
+		cout<<"Pierwsza cyfra jest nieparzysta"<<endl;
+	}else{	
+		cout<<"Pierwsza cyfra jest nieparzysta"<<endl;
+	}
 	return 0;
 }
